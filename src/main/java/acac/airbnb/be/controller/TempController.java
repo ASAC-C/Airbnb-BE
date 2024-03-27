fa42c7f89bfc11eb0864948619964fe553ffec84
luda412 <201810791@sangmyung.kr>
2024-03-27 14:54:00 +0900

    [FEAT]-초기설정

--- src/main/java/acac/airbnb/be/controller/TempController.java
+++ src/main/java/acac/airbnb/be/controller/TempController.java
@@ -1,7 +1,12 @@
 package acac.airbnb.be.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TempController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "hello";
+    }
 }
