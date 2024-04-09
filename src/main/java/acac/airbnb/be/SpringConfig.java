a4ae17ab471e4bc8152f8bbe7119485f14e058bb
luda412 <201810791@sangmyung.kr>
2024-04-09 09:38:58 +0900

    [FIX]-주석제거

--- src/main/java/acac/airbnb/be/SpringConfig.java
+++ src/main/java/acac/airbnb/be/SpringConfig.java
@@ -1,28 +1,8 @@
 package acac.airbnb.be;
 
-import acac.airbnb.be.repository.JdbcMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import acac.airbnb.be.repository.MemberRepository;
-import acac.airbnb.be.service.MemberService;
 
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-//    private DataSource dataSource;
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-//
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
-//        return new JdbcMemberRepository(dataSource);
-//    }
 }
