d1fc06919d09f4c502d401a1551ad3842cafccca
yunsh0712 <yunsh0712@naver.com>
2024-04-10 15:31:38 +0900

    [FIX] 충돌수정

--- src/main/java/acac/airbnb/be/controller/ProfileController.java
+++ src/main/java/acac/airbnb/be/controller/ProfileController.java
@@ -3,27 +3,20 @@ package acac.airbnb.be.controller;
 import acac.airbnb.be.domain.ProfileDto;
 import acac.airbnb.be.service.PorfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ProfileController {
 
     private final PorfileService porfileService;
 
     @GetMapping("/profiles/{id}")
-    public String profile(@PathVariable Long id, Model model){
-        Optional<ProfileDto> profileDto = porfileService.findOne(id);
-        System.out.println("컨트롤러 로그:" + profileDto);
-        model.addAttribute("profile", profileDto);
-        return "profileList.html";
+    public ProfileDto profile(@PathVariable Long id){
+        return  porfileService.findOne(id)
+                .orElseThrow(() -> new RuntimeException("프로필을 찾을 수 없음")); // 프로필이 존재하지 않으면 예외 발생
     }
-
 }
