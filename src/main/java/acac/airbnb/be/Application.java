a892d380716a251747501706025db6d784e92413
yunsh0712 <yunsh0712@naver.com>
2024-04-10 15:17:27 +0900

    [FIX] - 충돌수정

--- src/main/java/acac/airbnb/be/Application.java
+++ src/main/java/acac/airbnb/be/Application.java
@@ -2,8 +2,12 @@ package acac.airbnb.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication //스프링 부트 애플리케이션을 구성하는 데 사용됩니다.
+@EnableJpaAuditing //JPA Auditing 기능을 활성화합니다.
+@ServletComponentScan //서블릿 관련 컴포넌트를 자동으로 스캔하여 등록합니다.
 public class Application {
 
 	public static void main(String[] args) {
